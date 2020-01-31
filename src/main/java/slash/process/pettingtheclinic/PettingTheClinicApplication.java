9173d86b962b14ec94d3b0f5ed9b0e83bae9185f
slashuzero <duprat.c@live.com>
2020-01-31 14:45:53 +0100

    initial project add

---
+++ src/main/java/slash/process/pettingtheclinic/PettingTheClinicApplication.java
@@ -0,0 +1,13 @@
+package slash.process.pettingtheclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettingTheClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PettingTheClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/slash/process/pettingtheclinic/PettingTheClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package slash.process.pettingtheclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettingTheClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
