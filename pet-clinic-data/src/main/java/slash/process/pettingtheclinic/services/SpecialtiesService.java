0973ff55340e4632a163f52b2a86326463b0a61a
slashuzero <duprat.c@live.com>
2020-02-05 19:32:03 +0100

    Added Specialty Map impl for context . Closes #24

---
+++ pet-clinic-data/src/main/java/slash/process/pettingtheclinic/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package slash.process.pettingtheclinic.services;
+
+import slash.process.pettingtheclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+
+}
